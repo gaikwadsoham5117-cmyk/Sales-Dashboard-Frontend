75eb61abc0c5ab827c9fc0344c26db37255b57c4
Soham Gaikwad <gaikwadsoham5117@gmail.com>
2026-09-08 11:16:05 +0530

    Update Vite proxy for Render backend

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
 
     proxy: {
       '/api': {
-        target: 'http://localhost:8088',
+        target: 'https://sales-dashboard-backend-lo96.onrender.com' /*'http://localhost:8088'*/,
         changeOrigin: true,
         secure: false,
       }
